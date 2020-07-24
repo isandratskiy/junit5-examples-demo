e6f2320cf24006d780c4064b9e3720eac8b5895c
isandratskiy <i.sandrackii@gmail.com>
2020-07-25 01:25:29 +0300

    cleanup

--- src/main/java/service/PersonService.java
+++ src/main/java/service/PersonService.java
@@ -9,26 +9,23 @@ import static com.google.common.flogger.FluentLogger.forEnclosingClass;
 @AllArgsConstructor
 public class PersonService {
     private static final FluentLogger log = forEnclosingClass();
-    private PersonModel person;
+    private final PersonModel person;
 
     public void takeVacation() {
         this.person.setVacation(true);
         log.atInfo().log(
-                this.person.getFirstName() + " take vacation"
-        );
+                this.person.getFirstName() + " take vacation");
     }
 
     public void completeVacation() {
         this.person.setVacation(false);
         log.atInfo().log(
-                this.person.getFirstName() + " is out of vacation"
-        );
+                this.person.getFirstName() + " is out of vacation");
     }
 
     public PersonModel getPersonInformation() {
         log.atInfo().log(
-                "Information ==> " + this.person.toString()
-        );
+                "Information ==> " + this.person.toString());
         return this.person;
     }
 }
